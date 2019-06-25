7b29ea04055fd97fe5335af89057d4b7b3f3bac2
Dalton Solano dos Reis <dalton.reis@gmail.com>
2019-06-25 17:34:47 -0300

    Ajustes: retirou a data de finalização da atividade e mostra pontuação final do aluno.

--- src/app/pages/aluno-atividade-abrir/aluno-atividade-abrir.page.ts
+++ src/app/pages/aluno-atividade-abrir/aluno-atividade-abrir.page.ts
@@ -1,9 +1,6 @@
-import { ITurmaAlunoResposta } from './../../models/turma-aluno-resposta';
-import { ITurma } from './../../models/turma';
-import { QuestaoService } from 'src/app/services/questao.service';
-import { IQuestao } from 'src/app/models/questao';
 import { AlunoService } from './../../services/aluno.service';
 import { AutenticacaoService } from './../../services/autenticacao.service';
+import { AtividadeAlunoService } from './../../services/atividade-aluno.service';
 import { IAtividade } from './../../models/atividade';
 import { IResposta } from './../../models/resposta';
 import { AtividadeService } from './../../services/atividade.service';
@@ -11,9 +8,6 @@ import { NavController, LoadingController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { RespostaService } from 'src/app/services/resposta.service';
-import { TurmaAlunoService } from 'src/app/services/turma-aluno.service';
-import { TurmaService } from 'src/app/services/turma.service';
-import { TurmaAlunoRespostaService } from 'src/app/services/turma-aluno-resposta.service';
 
 @Component({
   selector: 'app-aluno-atividade-abrir',
@@ -23,59 +17,42 @@ import { TurmaAlunoRespostaService } from 'src/app/services/turma-aluno-resposta
 export class AlunoAtividadeAbrirPage implements OnInit {
   
   atividade: IAtividade = {
-    id: '',
+    nome: '',
     professor: '', //FK
-    disciplina: '',
-    nome: '',
-    dataTermino: new Date(),
-    dataCriacao: new Date()
-  }
-  
-  questaoCorrente: IQuestao = {
-    id: '',
-    atividade: null, //FK
-    apelido: '',
     problema: '',
-    dataCriacao: new Date(),
-  }
-  
-  turma: ITurma = {
-    id: '',
-    atividade: '', //FK
-    nome: '',
-    token: '',
-    dataCriacao: new Date(),
-    atividadeIniciada: false,
-    atividadeFinalizada: false,
-    questaoAtual: ''
-  }
-  
-  turmaAlunoResp: ITurmaAlunoResposta = {
-    turma: '', //FK
-    turmaAluno: '', //FK
-    resposta: '' //FK
+    tipo: '',
+    dataTermino: new Date(),
+    iniciada: false,
+    codigo: ''
   }
   
   usuarioCorrente = null;
-  idTurma = null;
-  questoesFinalizadas: Questao[] = [];
-  respostas: IResposta[] = [];
+  idAtividade= null;
+  respostas: IResposta[];
   segmentAtividade = 'edicao';
-  alunoVotou: boolean = false;
+  mostraBotaoVotar: boolean = false;
   
   constructor(private route: ActivatedRoute, private nav: NavController, 
-    private loadingController: LoadingController, private autenticacaoService: AutenticacaoService, 
-    private atividadeService: AtividadeService, private turmaService: TurmaService,
-    private turmaAlunoService: TurmaAlunoService, private alunoService: AlunoService,
-    private questaoService: QuestaoService, private respostaService: RespostaService,
-    private turmaAlunoRespostaService: TurmaAlunoRespostaService) { }
+    private loadingController: LoadingController,
+    private atividadeService: AtividadeService, private respostaService: RespostaService,
+    private atividadeAlunoService: AtividadeAlunoService, private alunoService: AlunoService,
+    private autenticacaoService: AutenticacaoService) { }
     
     ngOnInit() {
-      this.idTurma = this.route.snapshot.params['turma'];
+      let codigo = this.route.snapshot.params['codigo'];
       
-      if (this.idTurma) {
-        this.alunoVotou = false;
-        this.load();
+      if(codigo) {
+        this.atividadeService.getByCodigo(codigo).then((resultado) => {
+          this.idAtividade = resultado[0].id;
+          
+          if(this.idAtividade) {
+            this.load();
+            
+            this.respostaService.getByAtividade(this.idAtividade).subscribe((resultado) => {
+              this.respostas = resultado;
+            });
+          }
+        });
       }
     }
     
@@ -85,81 +62,48 @@ export class AlunoAtividadeAbrirPage implements OnInit {
       });
       await loading.present();
       
-      this.turmaService.get(this.idTurma).subscribe((turma) => {
-        this.atividadeService.get(turma.atividade).subscribe(atividade => {
-          loading.dismiss();
-          
-          this.atividade = atividade;
-          this.turma = turma;
-          this.usuarioCorrente = this.autenticacaoService.getID();
-          
-          if (turma.questaoAtual) {
-            document.getElementById('div_aguarde').style.display = "none";
-            document.getElementById('div_apresentacao').style.visibility = "visible";
+      this.atividadeService.get(this.idAtividade).subscribe(resultado => {
+        loading.dismiss();
+        
+        this.atividade = resultado;
+        
+        this.usuarioCorrente = this.autenticacaoService.getID();
+        this.alunoService.getByUsuario(this.usuarioCorrente).then((aluno) =>{
+          this.atividadeAlunoService.getByAtividadeAlunoVoto(this.idAtividade, aluno[0].id)
+          .subscribe((resultado) => {
+            //se já respondeu deixa o botão 'Votar' bloqueado
+            this.mostraBotaoVotar = (resultado.length > 0);
+          });
+        });
+      });
+    }
+    
+    async votar(idResposta: string, resposta: IResposta) {
+      this.usuarioCorrente = this.autenticacaoService.getID();
+      this.alunoService.getByUsuario(this.usuarioCorrente).then((aluno) =>{
+        this.atividadeAlunoService.getByAtividadeAlunoVoto(resposta.atividade, aluno[0].id)
+        .subscribe((resultado) => {
+          if (resultado.length > 0) {
+            resposta.votos++;
             
-            this.questaoService.get(turma.questaoAtual).subscribe((questao) => {
-              this.questaoCorrente = questao;
-              this.respostaService.getByQuestao(turma.questaoAtual).subscribe((respostas) => {
-                this.respostas = respostas;
+            this.respostaService.update(idResposta, resposta).then(() => {
+              resultado[0].alunoVotou = true;
+              resultado[0].alunoPontuacao = resposta.pontuacao;
+              
+              this.atividadeAlunoService.update(resultado[0].id, resultado[0]).then(() =>{
+
+                //VER COM O DALTON
+                // if (resposta.correta)
+                //   resposta.pontuacao += 10;
+
+                aluno[0].pontuacao += resposta.pontuacao;
+                aluno[0].nivel = Math.floor(aluno[0].pontuacao / 100);
+
+                console.log('pontuacao', aluno[0].pontuacao);
+                console.log('nivel', aluno[0].nivel);
                 
-                this.alunoService.getByUsuario(this.usuarioCorrente).then((aluno) =>{
-                  this.turmaAlunoService.getByTurmaAluno(this.idTurma, aluno[0].id)
-                  .subscribe((turmaAluno) => {
-                    this.alunoVotou = false;
-                    respostas.forEach(f => {
-                      this.turmaAlunoRespostaService.getByTurmaAlunoResposta(turmaAluno[0].id, f.id)
-                      .subscribe((resultado) => {
-                        //se já respondeu deixa o botão 'Votar' bloqueado
-                        if (!this.alunoVotou) {
-                          this.alunoVotou = (resultado.length > 0);
-                        }
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          } 
-          else if (turma.atividadeFinalizada) {
-            document.getElementById('div_aguarde').style.display = "none";
-            document.getElementById('div_apresentacao').style.display = "none";
-            document.getElementById('div_atividadeFinalizada').style.visibility = "visible";
-            
-            this.alunoService.getByUsuario(this.usuarioCorrente).then((aluno) =>{
-              this.turmaAlunoService.getByTurmaAluno(this.idTurma, aluno[0].id)
-              .subscribe((turmaAluno) => {
-                this.questaoService.getByAtividadeAsc(turma.atividade).subscribe((questoes) => {
-                  questoes.forEach(f => {
-                    if (this.questoesFinalizadas.findIndex(c => c.id == f.id) < 0) {
-                      let quest = new Questao();
-                      quest.id = f.id;
-                      quest.apelido = f.apelido;
-                      quest.problema = f.problema;
-                      
-                      this.respostaService.getByQuestao(f.id).subscribe((respostas) => {
-                        respostas.forEach(fr => {
-                          
-                          if (quest.respostas.findIndex(c => c.id == fr.id) < 0) {
-                            let resp = new Resposta();
-                            resp.id = fr.id;
-                            resp.correta = fr.correta;
-                            resp.conteudo = fr.conteudo;
-                            resp.pontuacao = fr.pontuacao;
-                            resp.escolhida = false;
-                            
-                            this.turmaAlunoRespostaService.getByTurmaAlunoRespostaPromise(turmaAluno[0].id, fr.id).then((resultado) => {
-                              if (resultado[0]) {
-                                resp.escolhida = true;
-                              }
-                            });
-                            quest.respostas.push(resp);
-                          }
-                        });
-                      });
-                      
-                      this.questoesFinalizadas.push(quest);
-                    }
-                  });
+                this.alunoService.update(aluno[0].id, aluno[0]).then(()=> {
+                  this.nav.navigateForward('/aluno-atividade-abrir/' + resposta.atividade.substring(0, 7).toUpperCase());
                 });
               });
             });
@@ -167,72 +111,5 @@ export class AlunoAtividadeAbrirPage implements OnInit {
         });
       });
     }
-    
-    async votar(idResposta: string, resposta: IResposta) {
-      //criar um registro na tabela turma-aluno-respostas
-      //guardar a pontuação na tabela turma-aluno
-      const loading = await this.loadingController.create({
-        message: 'Enviando'
-      });
-      await loading.present();
-
-      this.usuarioCorrente = this.autenticacaoService.getID();
-      this.alunoService.getByUsuario(this.usuarioCorrente).then((aluno) =>{
-        this.turmaAlunoService.getByTurmaAluno(this.idTurma, aluno[0].id)
-        .subscribe((turmaAluno) => {
-          this.turmaAlunoRespostaService.getByTurmaAlunoResposta(turmaAluno[0].id, idResposta)
-          .subscribe((turmaResposta) => {
-            if (turmaResposta.length <= 0) {
-              
-              this.turmaAlunoResp.resposta = idResposta;
-              this.turmaAlunoResp.turmaAluno = turmaAluno[0].id;
-              this.turmaAlunoResp.turma = turmaAluno[0].turma;
-              this.turmaAlunoRespostaService.add(this.turmaAlunoResp).then(() => {
-                
-                turmaAluno[0].alunoPontuacao += resposta.pontuacao;
-                this.turmaAlunoService.update(turmaAluno[0].id, turmaAluno[0]).then(() =>{
-                  
-                  aluno[0].pontuacao += resposta.pontuacao;
-                  aluno[0].nivel = Math.floor(aluno[0].pontuacao / 100);
-                  
-                  this.alunoService.update(aluno[0].id, aluno[0]).then(() => {
-                    loading.dismiss();
-                  });
-                });
-              });
-            }
-          });
-        });
-      });
-    }
   }
   
-  export class Questao {
-    id: string;
-    apelido: string;
-    problema: string;
-    respostas: Resposta[] = [];
-    
-    public Questao() {
-      this.id = '';
-      this.apelido = '';
-      this.problema = '';
-      this.respostas = [];
-    }
-  }
-  
-  export class Resposta {
-    id: string;
-    correta: boolean;
-    conteudo: string;
-    pontuacao: number;
-    escolhida: boolean;
-    
-    public Resposta() {
-      this.id = '';
-      this.correta = false;
-      this.conteudo = '';
-      this.pontuacao = 0;
-      this.escolhida = false;
-    }
-  }
